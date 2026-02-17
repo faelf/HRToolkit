6a7b68bdf69da6af0139e92574a1df48fccb19bc
Rafael Ferreira <rafaelf_s@hotmail.com>
2026-02-17 16:23:17 +0000

    feat: add min and max to date inputs

--- src/utilities/formatters.js
+++ src/utilities/formatters.js
@@ -9,4 +9,23 @@ export const formatters = {
     };
     return d.toLocaleDateString("en-GB", options);
   },
+  setMinMaxDates() {
+    const dateInputs = document.querySelectorAll('input[type="date"]');
+
+    const today = new Date();
+
+    const minDate = new Date(today);
+    minDate.setFullYear(today.getFullYear() - 60);
+
+    const maxDate = new Date(today);
+    maxDate.setFullYear(today.getFullYear() + 1);
+
+    const min = minDate.toISOString().split("T")[0];
+    const max = maxDate.toISOString().split("T")[0];
+
+    dateInputs.forEach((input) => {
+      input.min = min;
+      input.max = max;
+    });
+  },
 };
