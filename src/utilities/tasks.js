6707ce0a95169ce0aceb051b9ca9f0c01d171393
Rafael Ferreira <rafaelf_s@hotmail.com>
2026-05-25 20:51:13 +0000

    feat: add empty state container for tasks

--- src/utilities/tasks.js
+++ src/utilities/tasks.js
@@ -1,4 +1,17 @@
 export const tasks = {
+  _emptyContainer(text) {
+    const container = document.createElement("div");
+    container.className = "empty-state";
+    container.innerHTML = /* html */ `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <circle cx="12" cy="12" r="10"/>
+          <path d="m15 9-6 6"/>
+          <path d="m9 9 6 6"/>
+        </svg>
+        <p class="h1">${text}</p>
+      `;
+    return container;
+  },
   get(candidate) {
     const tasks = [];
     if (!candidate.references) {
@@ -59,20 +72,7 @@ export const tasks = {
     const tasks = this.getGrouped(data);
 
     if (tasks.length === 0) {
-      const emptyTasks = document.createElement("div");
-      emptyTasks.className = "empty-tasks";
-      emptyTasks.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
-          <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
-          <path d="M12 11h4" />
-          <path d="M12 16h4" />
-          <path d="M8 11h.01" />
-          <path d="M8 16h.01" />
-        </svg>
-        <span class="empty-text">No tasks</span>
-      `;
-      return emptyTasks;
+      return this._emptyContainer("No Tasks");
     }
 
     const ul = document.createElement("ul");
@@ -117,20 +117,7 @@ export const starters = {
     const starters = data;
 
     if (starters.length === 0) {
-      const empty = document.createElement("div");
-      empty.className = "empty-starters";
-      empty.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
-          <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
-          <path d="M12 11h4" />
-          <path d="M12 16h4" />
-          <path d="M8 11h.01" />
-          <path d="M8 16h.01" />
-        </svg>
-        <span class="empty-text">No starters</span>
-      `;
-      return empty;
+      return this._emptyContainer("No Starters");
     }
 
     const ul = document.createElement("ul");
