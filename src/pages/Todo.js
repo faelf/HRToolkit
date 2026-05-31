64d95c32e93fa22d825a33cb5c252e2c36cad3eb
Rafael Ferreira <rafaelf_s@hotmail.com>
2026-05-31 16:17:22 +0000

    feat: to do page

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,9 +1,16 @@
 import ToDoHTML from "../html/todo.html?raw";
+import { storages } from "../utilities/storages.js";
+import { DashboardInfo } from "../utilities/tasks.js";
 
 export const ToDoPage = {
   title: "HR Helper - To Do",
   html: ToDoHTML,
-  setup() {
-    // Page logic will go here
+  async setup() {
+    const allTasksContainer = document.getElementById("all-tasks");
+    if (!allTasksContainer) return;
+
+    const candidates = await storages.load("candidates");
+    allTasksContainer.innerHTML = "";
+    allTasksContainer.appendChild(DashboardInfo.tasks.allTasksList(candidates));
   },
 };
